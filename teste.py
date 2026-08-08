430554040300c4057c191cbe7922b9fa0e1dfabf
Ricardo Borges <rbs.rio@gmail.com>
2026-08-08 12:12:38 -0300

    Primeiro commit de exemplo

--- teste.py
+++ teste.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+class Employee:
+    def __init__(self, name, setor,salary):
+        self.name = name
+        self.salary = salary
+        self.setor = setor
+
+    def __str__(self):
+        return f'{self.name} recebe R$ {self.salary:.2f}'
+
+    def proporcional(self,days):
+        return self.salary / 30 * days
+
+joao = Employee('João', "TI", 3000)
+print(joao.name)
+print(joao.setor)
+print(joao.proporcional(1))
