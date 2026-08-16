e36fb67173e9438a65c2b2f99ad5d1f0e6b5648b
Ricardo Borges <rbs.rio@gmail.com>
2026-08-16 17:12:22 -0300

    adicionando o método __eq__ para comparar dois objetos Point

--- teste6.py
+++ teste6.py
@@ -4,10 +4,15 @@ class Point:
         self.y = y
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
     def __str__(self):
         return f"Point({self.x:}, {self.y:})"
 
 p1 = Point(1, 2)
 p2 = Point(3, 4)
 p3 = p1 + p2
+print(p1 == p2)
 print(p3)
