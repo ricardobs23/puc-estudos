4aeb0792c92c18a9671630a4dd7d11ee35002eca
Ricardo Borges <rbs.rio@gmail.com>
2026-08-17 11:55:06 -0300

    Novos exemplos das aulas

--- teste6.py
+++ teste6.py
@@ -6,7 +6,10 @@ class Point:
         return Point(self.x + other.x, self.y + other.y)
     
     def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        else:
+            raise TypeError("Para comparar, o outro objeto deve ser um ponto.")
 
     def __str__(self):
         return f"Point({self.x:}, {self.y:})"
@@ -14,5 +17,20 @@ class Point:
 p1 = Point(1, 2)
 p2 = Point(3, 4)
 p3 = p1 + p2
-print(p1 == p2)
 print(p3)
+
+"""""
+p = Point(3, 4)
+print(p == "teste!")
+
+print(p is p2)
+"""
+
+try:
+   p = Point(12,45)
+   s = "hello"
+   if p == s:
+      print("IGUAIS!")
+except TypeError:
+   print("Ocorreu um erro durante a execucao!")
+print("**Fim**")
