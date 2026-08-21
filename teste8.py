414d039c6646850aed441ac888884dccc7c81547
Ricardo Borges <rbs.rio@gmail.com>
2026-08-21 18:30:23 -0300

    Atualização

--- teste8.py
+++ teste8.py
@@ -3,6 +3,13 @@
 
 f = open("C:\\Repositorio\\puc-estudos\\websin.csv", encoding="utf-8")
 
+antiga = ""
 for line in f:
+    if antiga == "" or antiga < line.strip().split(";")[4]:
+       antiga = line.strip().split(";")[4]
     print(line.strip().split(";"))
+
+print(antiga)
+
 f.close()
+
